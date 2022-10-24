50282e36a7fa697c00ecb187dedf013377d2690c
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2022-10-24 10:27:06 +0530

    updated dropdown box

--- packages/frontend/src/components/DropDown.jsx
+++ packages/frontend/src/components/DropDown.jsx
@@ -4,18 +4,12 @@ import ReactDOM from "react-dom";
 export default function DropDown() {
   return (
     <select className="rounded-md h-8 w-60 bg-slate-400 text-black">
-      <option value="grapefruit" className="text-black">
-        Electronics
-      </option>
-      <option value="lime" className="text-black">
-        Furniture
-      </option>
-      <option selected value="coconut" className="text-black">
+      <option className="text-black">Electronics</option>
+      <option className="text-black">Furniture</option>
+      <option selected className="text-black">
         Jewelry
       </option>
-      <option value="mango" className="text-black">
-        Reat Estate
-      </option>
+      <option className="text-black">Real Estate</option>
     </select>
   );
 }
