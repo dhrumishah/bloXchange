d725d089794e693d9e04d673e65f68e40f3442c9
Aditya Gupta <adityagupta765469@gmail.com>
2022-11-14 12:19:38 +0530

    changed ui for search

--- packages/frontend/src/components/Orders/Orders.jsx
+++ packages/frontend/src/components/Orders/Orders.jsx
@@ -10,8 +10,8 @@ const Orders = () => {
             {/* <span className="text-xs">All products item</span> */}
           </div>
           <div className="flex items-center justify-between">
-            <div className="flex bg-gray-50 items-center p-2 rounded-md">
-              <svg
+            
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 text-gray-400"
                 viewBox="0 0 20 20"
@@ -21,16 +21,15 @@ const Orders = () => {
                   fillRule="evenodd"
                   d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                   clipRule="evenodd"
-                />
-              </svg>
+                /> */}
+              {/* </svg> */}
               <input
-                className="bg-gray-50 outline-none ml-1 block "
                 type="text"
                 name=""
                 id=""
-                placeholder="search..."
+              placeholder="search..."
+              className = "outline-none px-4 py-2 font-medium rounded-[10px] w-full dark:bg-[#242742] text-white"
               />
-            </div>
           </div>
         </div>
         <TabsRender />
